4d85acedf30022f7f42403b65cb09afa5dffbd15
Uwaoma Cyruz <uwaomacyruz@gmail.com>
2023-05-02 18:05:29 -0700

    added a background image

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+const TypewriterMultiLine = ({ lines }) => {
+  const [lineIndex, setLineIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (charIndex < lines[lineIndex].text.length - 1) {
+        setCharIndex(charIndex + 1);
+      } else if (lineIndex < lines.length - 1) {
+        setLineIndex(lineIndex + 1);
+        setCharIndex(0);
+      }
+    }, lines[lineIndex].delay || 150);
+
+    return () => clearTimeout(timer);
+  }, [charIndex, lineIndex, lines]);
+
+  useEffect(() => {
+    if (
+      lineIndex === lines.length - 1 &&
+      charIndex === lines[lineIndex].text.length - 1
+    ) {
+      setTimeout(() => {
+        setLineIndex(0);
+        setCharIndex(0);
+      }, 1000); // wait for 1 second before resetting
+    }
+  }, [charIndex, lineIndex, lines]);
+
+  return (
+    <>
+      {lines.map((line, index) => (
+        <h3
+          className="name"
+          key={index}
+          style={line.style || {}} // pass in style prop for each line
+        >
+          {index <= lineIndex
+            ? // render each line up to the current lineIndex
+              line.text.slice(0, charIndex + 1)
+            : null}
+        </h3>
+      ))}
+    </>
+  );
+};
+
 function Home() {
   return (
     <div id="home">
       <div className="topnav">
         <img
-          src="https://i.pinimg.com/564x/c0/b0/03/c0b003d4818fe78de1b940886a539f33.jpg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5SQntNi68ASSiKFpRVzQOMfJCx5aYevH69w&usqp=CAU"
           width="100px"
           alt="tech"
+          style={{ borderRadius: "50%", margin: "15px" }}
         />
         <a href="#contact">Contact</a>
         <a href="#project">Project</a>
@@ -17,10 +67,19 @@ function Home() {
 
       <div class="flex flex3">
         <div class="flex-items">
-          <p>Hey there, I am </p>
+          <div>
+            <TypewriterMultiLine
+              lines={[
+                {
+                  text: "Hey there, I am",
+                },
+                { text: "Uwaoma Cyrus   " },
+              ]}
+            />
+          </div>
 
-          <h3 className="name">Uwaoma Cyrus</h3>
-          <h4>I'm a software Engineer</h4>
+          {/* <h3 className="name">Uwaoma Cyrus</h3>
+          <h4>I'm a software Engineer</h4> */}
           <h4> I build and design Beautiful user Interfaces</h4>
           <a className="mailButton" href="mailto:uwaomacyruz@gmail.com">
             Send Me An Email
--- src/about.js
+++ src/about.js
@@ -1,69 +1,101 @@
-import rlogo from "./icons/react-icon.jpg";
-import cslogo from "./icons/css3_icon.webp";
-import tlogo from "./icons/typescript.webp";
-import hlogo from "./icons/html5-icon.png";
-import vlogo from "./icons/vue-logo.png";
+import { FaReact as ReactIcon } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaVuejs } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+
+const IconText = ({ icon, text }) => {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      {icon}
+      <p style={{ textAlign: "center" }}>{text}</p>
+    </div>
+  );
+};
 
 function About() {
   return (
     <div className="box">
       <h1 id="about">About Me</h1>
-
+      <div></div>
       <div className="About-Section">
-        <p>
+        <p className="text-format">
           I started out as a self-taught developer, trying to find the best
-          possible way to improve my learning Experience.
+          possible way to improve my learning Experience. working with other
+          developers, building Projects, joining communities and contributing to
+          open source has been a sure fire way to improving my skill and keep me
+          up to date with latest technologies in software. Among my exciting
+          tech journey and experience is the Privledge of passing through
+          altSchool with a diploma in Frontend Engineering With the increase in
+          Cyber-Attack and software vulnurabilities I am Currenctly taking a
+          course on cyber security, so as to better secure any organization I
+          work for. I have a keen Interest in developing efficient and reliable
+          solutions while meeting user expectation and satisfaction{" "}
         </p>
-        <p>
-          working with other developers, building Projects, joining communities
-          and contributing to open source has been a sure fire way to improving
-          my skill and keep me up to date with latest technologies in software.
-        </p>
-        <p>
-          Among my exciting tech journey and experience is the Privledge of
-          passing through altSchool with a diploma in Frontend Engineering
-        </p>
-        <p>
-          With the increase in Cyber-Attack and software vulnurabilities I am
-          Currenctly taking a course on cyber security, so as to better secure
-          any organization I work for.
-        </p>
-        <p>
-          I have a keen Interest in developing efficient and reliable solutions
-          while meeting user expectation and satisfaction{" "}
-        </p>
-        <h2 style={{ marginLeft: "25px" }}>Here are few technologies I use</h2>
-        <div className="grid-container">
-          <div>
-            <img src={hlogo} alt="react" width="80px" height="100px" />
-          </div>
-          <div>
-            <img src={cslogo} alt="react" width="70px" height="100px" />
-          </div>
-          <div>
-            <img src={rlogo} alt="react" width="90px" />
-          </div>
-          <div>
-            <img src={tlogo} alt="react" width="90px" height="108px" />
-          </div>
-          <div>
-            <img
-              src={vlogo}
-              style={{ marginBottom: "15px", border: "none" }}
-              alt="react"
-              width="90px"
-              height="100px"
-            />
-          </div>
-          <div className="iteml">Html5</div>
-
-          <div className="iteml">css3</div>
-
-          <div className="item">Reactjs</div>
-
-          <div className="item">Typescript</div>
-
-          <div className="item">Vuejs</div>
+        <h2 style={{ marginLeft: "25px", marginBottom: "25px" }}>
+          Here are few technologies I use
+        </h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <IconText
+            icon={
+              <FaHtml5
+                size={32}
+                style={{
+                  color: "red",
+                  backgroundColor: "white",
+                  margin: "10px",
+                }}
+              />
+            }
+            text="HTML5"
+          />
+          <IconText
+            icon={
+              <FaCss3Alt
+                size={32}
+                style={{
+                  color: "red",
+                  backgroundColor: "white",
+                  margin: "10px",
+                }}
+              />
+            }
+            text="CSS3"
+          />
+          <IconText
+            icon={
+              <ReactIcon
+                size={32}
+                style={{
+                  color: "red",
+                  backgroundColor: "white",
+                  margin: "10px",
+                }}
+              />
+            }
+            text="React"
+          />
+          <IconText
+            icon={
+              <SiTypescript
+                size={32}
+                style={{
+                  color: "red",
+                  backgroundColor: "white",
+                  margin: "10px",
+                }}
+              />
+            }
+            text="Typescript"
+          />
+          <IconText icon={<FaVuejs size={32} />} text="Vue" />
         </div>
       </div>
     </div>
